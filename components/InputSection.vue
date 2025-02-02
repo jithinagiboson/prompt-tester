7543e1c7c49bcd7081f178a2c5bd04c3f7ac6f40
jithiniboson <jithin@ibosoninnov.com>
2025-02-02 11:06:25 +0530

    Fix spinner bug

--- components/InputSection.vue
+++ components/InputSection.vue
@@ -7,7 +7,7 @@
     "
   >
     <h2 class="text-2xl font-bold mb-4">Input Section</h2>
-    <div v-if="currentTab?.isLoading"><div class="spinner"></div></div>
+    
     <div>
       <div class="mb-4">
         <label class="block text-gray-700 font-bold mb-2">Define Variables</label>
@@ -121,21 +121,4 @@ onMounted(() => {
 });
 </script>
 
-<style>
-.spinner {
-  border: 4px solid rgba(0, 0, 0, 0.1);
-  border-radius: 50%;
-  border-top: 4px solid #3498db;
 
-  animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -110,4 +110,21 @@ body {
 .flex-1 {
   flex: 1;
 }
+
+.spinner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  border-top: 4px solid #3498db;
+
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
